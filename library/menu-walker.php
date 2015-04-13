e869e100753ea68d07bc6dccdb837938d4b4541c
slateb <slate.ballard@gmail.com>
2015-04-13 10:17:13 -0400

    clean up nav

--- library/menu-walker.php
+++ library/menu-walker.php
@@ -3,40 +3,108 @@
  * Customize the output of menus for Foundation top bar
  */
 if ( ! class_exists( 'starterslab_Top_Bar_Walker' ) ) :
-class starterslab_Top_Bar_Walker extends Walker_Nav_Menu {
 
-	function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
-		$element->has_children = ! empty( $children_elements[ $element->ID ] );
-		$element->classes[] = ( $element->current || $element->current_item_ancestor ) ? 'active' : '';
-		$element->classes[] = ( $element->has_children && 1 !== $max_depth ) ? 'has-dropdown' : '';
-
-		parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
-	}
-
-	function start_el( &$output, $object, $depth = 0, $args = array(), $current_object_id = 0 ) {
-		$item_html = '';
-		parent::start_el( $item_html, $object, $depth, $args );
-
-		$output .= ( 0 == $depth ) ? '<li class="divider"></li>' : '';
-
-		$classes = empty( $object->classes ) ? array() : (array) $object->classes;
-
-		if ( in_array( 'label', $classes ) ) {
-			$output .= '<li class="divider"></li>';
-			$item_html = preg_replace( '/<a[^>]*>(.*)<\/a>/iU', '<label>$1</label>', $item_html );
-		}
-
-	if ( in_array( 'divider', $classes ) ) {
-		$item_html = preg_replace( '/<a[^>]*>( .* )<\/a>/iU', '', $item_html );
-	}
-
-		$output .= $item_html;
-	}
-
-	function start_lvl( &$output, $depth = 0, $args = array() ) {
-		$output .= "\n<ul class=\"sub-menu dropdown\">\n";
-	}
-
+/**
+ * Cleaner walker for wp_nav_menu()
+ *
+ * Walker_Nav_Menu (WordPress default) example output:
+ *   <li id="menu-item-8" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-8"><a href="/">Home</a></li>
+ *   <li id="menu-item-9" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9"><a href="/sample-page/">Sample Page</a></l
+ *
+ * Roots_Nav_Walker example output:
+ *   <li class="menu-home"><a href="/">Home</a></li>
+ *   <li class="menu-sample-page"><a href="/sample-page/">Sample Page</a></li>
+ */
+function add_filters($tags, $function) {
+  foreach($tags as $tag) {
+    add_filter($tag, $function);
+  }
 }
+function is_element_empty($element) {
+  $element = trim($element);
+  return empty($element) ? false : true;
+}
+
+class starterslab_Top_Bar_Walker extends Walker_Nav_Menu {
+  function check_current($classes) {
+    return preg_match('/(current[-_])|active|dropdown/', $classes);
+  }
+
+  function start_lvl(&$output, $depth = 0, $args = array()) {
+    $output .= "\n<ul class=\"dropdown\">\n";
+  }
+
+  function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
+    $item_html = '';
+    parent::start_el($item_html, $item, $depth, $args);
+
+    if ($item->is_dropdown && ($depth === 0)) {
+      $item_html = str_replace('<li', '<li class="has-dropdown not-click"', $item_html);
+      $item_html = str_replace('</a>', ' <b class="caret"></b></a>', $item_html);
+    }
+    elseif (stristr($item_html, 'li class="divider')) {
+      $item_html = preg_replace('/<a[^>]*>.*?<\/a>/iU', '', $item_html);
+    }
+    elseif (stristr($item_html, 'li class="dropdown-header')) {
+      $item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html);
+    }
+
+    $item_html = apply_filters('roots_wp_nav_menu_item', $item_html);
+    $output .= $item_html;
+  }
+
+  function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ) {
+        $element->has_children = !empty( $children_elements[$element->ID] );
+        $element->classes[] = ( $element->current || $element->current_item_ancestor ) ? 'active' : '';
+        $element->classes[] = ( $element->has_children && $max_depth !== 1 ) ? 'has-dropdown' : '';
+        
+        parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+    }
+}
+
+/**
+ * Remove the id="" on nav menu items
+ * Return 'menu-slug' for nav menu classes
+ */
+function roots_nav_menu_css_class($classes, $item) {
+  $slug = sanitize_title($item->title);
+  $classes = preg_replace('/(current(-menu-|[-_]page[-_])(item|parent|ancestor))/', 'active', $classes);
+  $classes = preg_replace('/^((menu|page)[-_\w+]+)+/', '', $classes);
+
+  $classes[] = 'menu-' . $slug;
+
+  $classes = array_unique($classes);
+
+  return array_filter($classes, 'is_element_empty');
+}
+add_filter('nav_menu_css_class', 'roots_nav_menu_css_class', 10, 2);
+add_filter('nav_menu_item_id', '__return_null');
+
+/**
+ * Clean up wp_nav_menu_args
+ *
+ * Remove the container
+ * Use Roots_Nav_Walker() by default
+ */
+function roots_nav_menu_args($args = '') {
+  $roots_nav_menu_args['container'] = false;
+
+  if (!$args['items_wrap']) {
+    $roots_nav_menu_args['items_wrap'] = '<ul class="%2$s">%3$s</ul>';
+  }
+
+  if (current_theme_supports('bootstrap-top-navbar') && !$args['depth']) {
+    $roots_nav_menu_args['depth'] = 3;
+  }
+
+  if (!$args['walker']) {
+    $roots_nav_menu_args['walker'] = new starterslab_Top_Bar_Walker();
+  }
+
+  return array_merge($args, $roots_nav_menu_args);
+}
+add_filter('wp_nav_menu_args', 'roots_nav_menu_args');
+
 endif;
 ?>
+
